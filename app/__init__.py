673d53cac5527ce7ac1267afd61baf859abde3b1
Chrisjkey2332 <Key.christopher@yahoo.com>
2022-09-28 13:26:07 -0500

    c

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,10 @@
+from distutils.command.config import config
 from flask import Flask
+from config import Config
 from .site.routes import site
 
 app = Flask(__name__)
 
 app.register_blueprint(site)
+
+app.config.from_object(Config)
